6d3b615b3c021191396b761c886ce25bef604a6e
cmlee <bal.dongdongdong@gmail.com>
2025-02-16 22:00:48 +0900

    :sparkles: Add CSS class name constants for slot counter components
    - Define CSS class names as constants in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -9,3 +9,13 @@ export const NBSP_UNICODE_REGEXP = /\u00A0/;
 
 // Regular expressions for narrow no-break space separator
 export const NARROW_NO_BREAK_SPACE_UNICODE_REGEXP = /\u202F/;
+
+// Class names
+export const CLASS_NAMES = {
+  SLOT_COUNTER: 'slot-counter',
+  SLOT_COUNTER_SEPARATOR: 'slot-counter-separator',
+  SLOT_COUNTER_ITEM: 'slot-counter-item',
+  SLOT_COUNTER_ITEM_NUMBERS: 'slot-counter-item-numbers',
+  SLOT_COUNTER_ITEM_NUMBER: 'slot-counter-item-number',
+  SLOT_COUNTER_ITEM_NUMBER_VALUE: 'slot-counter-item-number-value',
+};
